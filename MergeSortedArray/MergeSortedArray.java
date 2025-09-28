657fa51896ca2f7aa2f22f133cdb1a7a0c73f678
quocanh4433 <hoquocanh.td@gmail.com>
2025-09-28 12:25:13 +0700

    [java] merge sorted array

--- MergeSortedArray/MergeSortedArray.java
+++ MergeSortedArray/MergeSortedArray.java
@@ -3,12 +3,46 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
 
-    static void merge(int[] nums1, int m, int[] nums2, int n) {
+    /* SOLUITON đầu tiên nghĩ ra 
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;
         int j = n - 1;
         int k = m + n - 1;
 
         // So sánh từ cuối mảng, điền vào nums1 từ phải sang trái
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] <= nums2[j]) {
+                nums1[k] = nums2[j];
+                j--;
+            } else {
+                nums1[k] = nums1[i];
+                i--;
+            }
+
+            k--;
+        }
+
+        // lấy phần tử nums2 còn lại thêm vào trong mảng
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+    
+     */
+    /**
+     * SOLUTION code gọn hơn
+     *
+     *
+     * NHƯNG chưa hiểu lắm - cần nghiệm lại
+     */
+    static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
         while (j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j]) {
                 nums1[k] = nums1[i];
@@ -39,5 +73,32 @@ public class MergeSortedArray {
         int n_2 = 3;
         merge(nums1_2, m_2, nums2_2, n_2);
         System.out.println(Arrays.toString(nums1_2));
+    }
+}
+
+class Solution {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] <= nums2[j]) {
+                nums1[k] = nums2[j];
+                j--;
+            } else {
+                nums1[k] = nums1[i];
+                i--;
+            }
+
+            k--;
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
     }
 }
