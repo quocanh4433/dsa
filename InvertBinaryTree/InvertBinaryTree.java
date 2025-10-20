04bdd35416e41bb5439fef919a5f91217c0dc15c
quocanh4433 <hoquocanh.td@gmail.com>
2025-10-20 16:11:30 +0700

    invert binary tree

--- InvertBinaryTree/InvertBinaryTree.java
+++ InvertBinaryTree/InvertBinaryTree.java
@@ -1,7 +1,70 @@
 
 public class InvertBinaryTree {
 
+    /*
+     * BRAINSTORM
+     * 
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        while (true) {
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+            if (left != null || right != null) {
+                root.left = right;
+                root.right = left;
+                invertTree(root.left);
+                invertTree(root.right);
+            }
+            break;
+        }
+        return root;
+    }
+
+    /*
+     * OPTIMIZED
+     * 
+     */
+    public TreeNode invertTree_2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
     public static void main(String[] args) {
 
     }
+}
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
 }
