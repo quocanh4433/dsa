d048447e9041f9c2cfa1ad878684865edc1b1efc
quocanh4433 <hoquocanh.td@gmail.com>
2025-11-04 21:51:51 +0700

    revise: search in a binary search tree

--- TREE/SearchInABinarySearchTree/SearchInABinarySearchTree.java
+++ TREE/SearchInABinarySearchTree/SearchInABinarySearchTree.java
@@ -4,6 +4,12 @@ import common.TreeNode;
 
 public class SearchInABinarySearchTree {
 
+    /*
+     * SOLUTION 1: RECURSION
+     * 
+     * Time complexity: O(h)
+     * Space complexity: O(h) stak recursion
+     */
     public static TreeNode searchBST(TreeNode root, int val) {
         if (root == null) {
             return null;
@@ -16,6 +22,38 @@ public class SearchInABinarySearchTree {
         } else {
             return root;
         }
+    }
+
+    /*
+     * SOLUTION 1.1: RECURSION
+     * 
+     * Time complexity: O(h)
+     * Space complexity: O(h) stak recursion
+     */
+    public static TreeNode searchBST_2(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+
+        if (val > root.val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+
+
+    /*
+     * SOLUTION 2: ITERATIVE
+     * 
+     * Time complexity: O(h)
+     * Space complexity: O(1)
+     */
+    public static TreeNode searchBST_3(TreeNode root, int val) {
+        while (root != null && root.val != val) {
+            root = val < root.val ? root.left : root.right;
+        }
+        return root;
     }
 
     public static void main(String[] args) {
