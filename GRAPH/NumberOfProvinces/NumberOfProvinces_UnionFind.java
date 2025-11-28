65be3f8f6e4483a3bf90d1d5421fc1a3540bdd0a
quocanh4433 <hoquocanh.td@gmail.com>
2025-11-28 10:54:12 +0700

    number of provinces

--- GRAPH/NumberOfProvinces/NumberOfProvinces_UnionFind.java
+++ GRAPH/NumberOfProvinces/NumberOfProvinces_UnionFind.java
@@ -1,8 +1,76 @@
 package GRAPH.NumberOfProvinces;
 
 public class NumberOfProvinces_UnionFind {
-    
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        UnionFind uf = new UnionFind(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {// chỉ cần đi nửa trên
+                // Tai sao chỉ đi nửa trên ❓
+                // vì ma trận là đối xứng (isConnected[i][j] == isConnected[j][i]).
+                if (isConnected[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+
+        return uf.getCount();
+
+    }
+
     public static void main(String[] args) {
-        
+
+    }
+}
+
+class UnionFind {
+
+    private int[] parent;
+    private int[] rank;
+    private int count;
+
+    public UnionFind(int n) {
+        this.parent = new int[n];
+        this.rank = new int[n];
+        this.count = n;
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int findParent(int x) {
+        if (parent[x] != x) {
+            parent[x] = findParent(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = findParent(x);
+        int rootY = findParent(y);
+
+        if (rootX == rootY) {
+            return;
+        }
+
+        // Union by rank
+        if(rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else {
+            parent[rootX] = rootY;
+            rank[rootY]++;
+        }
+
+        count--;
+
+    }
+
+    public int getCount() {
+        return count;
     }
 }
--- NEWREADME.md
+++ NEWREADME.md
@@ -30,7 +30,7 @@
     1. [Shortest Path In Binary Matrix](https://github.com/quocanh4433/dsa/blob/main/GRAPH/ShortestPathInBinaryMatrix)
     1. [Graph Valid Tree (Only NeetCode)](https://github.com/quocanh4433/dsa/blob/main/GRAPH/GraphValidTree)
     1. [Number Of Islands](https://github.com/quocanh4433/dsa/blob/main/GRAPH/NumberOfIslands)
-    1. [Number Of Provinces (XEM LẠI SOLUTION UNION FIND) 🔥](https://github.com/quocanh4433/dsa/blob/main/GRAPH/NumberOfProvinces)
+    1. [Number Of Provinces (XEM LẠI SOLUTION UNION FIND - ĐÃ XEM NHƯNG VẪN CHƯA HIỂU) 🔥](https://github.com/quocanh4433/dsa/blob/main/GRAPH/NumberOfProvinces)
     1. [Number Of Operations To Connect Network (XEM LẠI) 🔥](https://github.com/quocanh4433/dsa/blob/main/GRAPH/NumberOfOperationsToConnectNetwork)
     1. [Number Of Nodes In The Sub-Tree With The Same Label (XEM LẠI) 🔥](https://github.com/quocanh4433/dsa/blob/main/GRAPH/NumberOfNodesInTheSubTreeWithTheSameLabel)
     1. [Max Area Of Island](https://github.com/quocanh4433/dsa/blob/main/GRAPH/MaxAreaOfIsland)
