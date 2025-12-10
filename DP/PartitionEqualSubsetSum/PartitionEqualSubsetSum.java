d081a5b0313fc55decd80ff792f3b93ef85a4203
quocanh4433 <hoquocanh.td@gmail.com>
2025-12-10 17:01:01 +0700

    patrition equal subset sum

--- DP/PartitionEqualSubsetSum/PartitionEqualSubsetSum.java
+++ DP/PartitionEqualSubsetSum/PartitionEqualSubsetSum.java
@@ -1,14 +1,17 @@
 public class PartitionEqualSubsetSum {
+    private int[] nums;
+
     /*
     RECURSION
     
     Time -> O(2^n)
     Space -> O(n)
+
+    BỊ LỖI TLE
     
     */
-    private int[] nums;
 
-    public boolean canPartition(int[] nums) {
+    public boolean canPartition_TLE(int[] nums) {
         this.nums = nums;
         int sum = 0;
         for (int num : nums) {
@@ -17,6 +20,46 @@ public class PartitionEqualSubsetSum {
         if (sum % 2 != 0) {
             return false;
         }
+        return dfs_TLE(0, sum / 2);
+    }
+
+    public boolean dfs_TLE(int i, int target) {
+        if (target == 0) {
+            return true;
+        }
+
+        if (i == nums.length || target < 0) {
+            return false;
+        }
+
+        boolean take = dfs_TLE(i + 1, target - nums[i]);
+        boolean skip = dfs_TLE(i + 1, target);
+
+        return take || skip;
+    }
+
+
+    /*
+    OPTIMIZE WITH MEMORIZATION
+
+    Time -> O(n * target)
+    Space -> O(n * target)
+    
+    
+    */
+    Boolean[][] memo;
+
+    public boolean canPartition(int[] nums) {
+        this.nums = nums;
+        int n = nums.length;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        memo = new Boolean[n][sum / 2 + 1];
         return dfs(0, sum / 2);
     }
 
@@ -29,10 +72,16 @@ public class PartitionEqualSubsetSum {
             return false;
         }
 
+        if (memo[i][target] != null) {
+            return memo[i][target];
+        }
+
         boolean take = dfs(i + 1, target - nums[i]);
         boolean skip = dfs(i + 1, target);
 
-        return take || skip;
+        memo[i][target] = take || skip;
+
+        return memo[i][target];
     }
 
 
