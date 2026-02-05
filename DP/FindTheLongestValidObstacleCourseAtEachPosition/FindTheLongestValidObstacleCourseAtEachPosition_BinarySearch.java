137bec1b61cfe845ed511c7b94db211514e39ac8
quocanh4433 <hoquocanh.td@gmail.com>
2026-02-05 22:01:49 +0700

    find the longest valid obstacle course at each position

--- DP/FindTheLongestValidObstacleCourseAtEachPosition/FindTheLongestValidObstacleCourseAtEachPosition_BinarySearch.java
+++ DP/FindTheLongestValidObstacleCourseAtEachPosition/FindTheLongestValidObstacleCourseAtEachPosition_BinarySearch.java
@@ -1,33 +1,45 @@
-public class FindTheLongestValidObstacleCourseAtEachPosition_BinarySearch {
-    /*
-        time limit exceeded ❌❌❌
 
-        time O(n**2)
+public class FindTheLongestValidObstacleCourseAtEachPosition_BinarySearch {
+
+    /*
+        time O(nlogn)
         sapce O(n)
 
-        1 <= n <= 105
-    */
+        1 <= n <= 10**5
+     */
 
     public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
         int n = obstacles.length;
-        int[] dp = new int[n];
+        int[] result = new int[n];
+        int[] tail = new int[n];
+        int size = 0; // đồ dài đang xét
 
         for (int i = 0; i < n; i++) {
-            dp[i] = 1;
+            int x = obstacles[i];
 
-            // kiểm tra có nối đến các obstsacle tăng dần
-            for (int j = 0; j < i; j++) {
-                if (obstacles[j] <= obstacles[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    // +1: công cho phân từ thư i
+            // tìm vị trí chèn x vào tail
+            // tìm phần tư đầu tiên lớn hơn x
+            int l = 0, r = size;
+            while (l < r) {
+                int m = (l + r) / 2;
+                if (tail[m] > x) {
+                    r = m;
+                } else {
+                    l = m + 1;
                 }
             }
+
+            tail[l] = x;
+            if (l == size) {
+                size++;
+            }
+            result[i] = l + 1;
         }
 
-        return dp;
+        return result;
     }
 
     public static void main(String[] args) {
-        
+
     }
 }
