2439d2548fed6cd9ff1770f659853c68c5d9a6f4
quocanh4433 <hoquocanh.td@gmail.com>
2026-02-27 08:46:53 +0700

    1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance

--- GRAPH/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance.java
+++ GRAPH/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance.java
@@ -1,6 +1,16 @@
 import java.util.*;
 
 class Solution {
+    /*
+        time: O(n * elogn)
+            ▪︎ build graph: O(e)
+            ▪︎ dijkstra: O(elogn)
+            ▪︎ duyện qua n node: O(n)
+        space: O(e + n)
+            ▪︎ graph: O(e + n)   
+            ▪︎ dist[]: O(n)
+            ▪︎ pq: O(n) 
+    */
 
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
         // build graph
@@ -15,14 +25,15 @@ class Solution {
             graph[e[1]].add(new int[]{e[0], e[2]});
         }
 
-        int city = -1; // thành phố cân trả về
+        int city = -1; // thành phố cần trả về
         int minReachable = Integer.MAX_VALUE; // số thành phố ít nhất tưng thấy 
 
         for (int i = 0; i < n; i++) {
-            // số thành phố có thể đi tới i
+            // số thành phố có thể đi bất đầu từ i thỏa điều kiện distanceThreshold
             int count = dijkstra(i, graph, distanceThreshold, n);
 
-            if (count <= minReachable) {
+            // tại sao điều kiện là <= mà không phải < ?
+            if (count <= minReachable) { 
                 minReachable = count;
                 city = i;
             }
@@ -31,12 +42,37 @@ class Solution {
         return city;
     }
 
-    public int dijkstra(int start; List<int[]>[] graph, int distanceThreshold, int n) {
+    public int dijkstra(int start, List<int[]>[] graph, int distanceThreshold, int n) {
         int[] dist = new int[n]; // dist[i]: khoảng cách ngắn nhất từ start tới i
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[start] = 0;
 
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.offer(new int[]{0, start});
 
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int d = curr[0];
+            int u = curr[1];
+
+            if (d > dist[u]) continue;
+
+            for (int[] edge : graph[u]) {
+                int v = edge[0];
+                int w = edge[1];
+                if (dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    pq.offer(new int[]{dist[v], v});
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != start && dist[i] <= distanceThreshold) {
+                count++;
+            }
+        }
+        return count;
     }
 }
