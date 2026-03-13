1f3d417aa48b9928eb5091d448efa033823292a8
quocanh4433 <hoquocanh.td@gmail.com>
2026-03-13 15:56:38 +0700

    124_binary_tree_maximum_path_sum

---
+++ [A]tree/[A]tree_dfs_postorder/124_binary_tree_maximum_path_sum/124_binary_tree_maximum_path_sum.java
@@ -0,0 +1,50 @@
+
+@SuppressWarnings("unused")
+class BinaryTreeMaximumPathSum {
+
+    /*
+        time O(n)
+        space O(h)
+            ▪︎ balanced tree: O(logn)
+            ▪︎ skew tree: O(n)
+
+        postorder + divide & conquer
+     */
+    int res = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    public int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = Math.max(0, dfs(node.left));
+        int right = Math.max(0, dfs(node.right));
+
+        res = Math.max(res, left + right + node.val);
+
+        return node.val + Math.max(left, right);
+    }
+}
+
+class TreeNode {
+
+    @SuppressWarnings("unused")
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    @SuppressWarnings("unused")
+    TreeNode() {
+    }
+
+    @SuppressWarnings("unused")
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+}
