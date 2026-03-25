865b1b422448ac124249192d52dab816d048de0e
quocanh4433 <hoquocanh.td@gmail.com>
2026-03-25 15:22:38 +0700

    141_linked_list_cycle

---
+++ [B]linked_list/141_linked_list_cycle/141_linked_list_cycle.java
@@ -0,0 +1,47 @@
+
+@SuppressWarnings("unused")
+class LinkedListCycle {
+
+    /*
+        time O(n)
+        space O(1)
+
+        ~ using 2 pointers: fast and slow
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+class ListNode {
+
+    @SuppressWarnings("unused")
+    int val;
+    ListNode next;
+
+    @SuppressWarnings("unused")
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    @SuppressWarnings("unused")
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
