06b9fdb43a014ecba109a6e0ca18ec7714795a2b
quocanh4433 <hoquocanh.td@gmail.com>
2026-03-26 08:36:18 +0700

    876_middle_of_the_linked_list

---
+++ [B]linked_list/876_middle_of_the_linked_list/876_middle_of_the_linked_list.java
@@ -0,0 +1,39 @@
+
+@SuppressWarnings("unused")
+class MiddleOfTheLinkedList {
+
+    /*
+        time O(n)
+        space O(1)
+
+        fast đi nhanh gấp đôi slow
+        khi fast đi hết n node thì slow đang ở n/2
+     */
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
+
+class ListNode {
+
+    @SuppressWarnings("unused")
+    int val;
+    ListNode next;
+
+    @SuppressWarnings("unused")
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    @SuppressWarnings("unused")
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
