1a61e7d6adda122e558cd5451bc8d65769eeaa5d
quocanh4433 <hoquocanh.td@gmail.com>
2026-04-05 23:55:44 +0700

    2583_kth_largest_sum_in_a_binary_tree

---
+++ [A]tree/[A]tree_bfs/2583_kth_largest_sum_in_a_binary_tree/2583_kth_largest_sum_in_a_binary_tree_sort.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+@SuppressWarnings("unused")
+class KthLargestSumInABinaryTree {
+
+    /*
+        time O(n long n)
+            ▪︎ bfs O(n)
+            ▪︎ sort O(nlogn)
+        space O(n) 
+    */
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        List<Long> list = new ArrayList<>();
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            long sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = q.poll();
+                sum += cur.val;
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+            }
+            list.add(sum);
+        }
+
+        Collections.sort(list); // O(nlogn) skewed tree
+
+        if (k > list.size()) {
+            return -1;
+        }
+        return list.get(list.size() - k);
+    }
+}
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    @SuppressWarnings("unused")
+    TreeNode() {
+    }
+
+    @SuppressWarnings("unused")
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+}
