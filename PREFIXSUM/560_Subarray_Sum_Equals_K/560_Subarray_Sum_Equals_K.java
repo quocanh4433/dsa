5fe0df3fd47dd5ef2c582cfe3a30fe40c8d52006
KILALA\anh.ho <anh.ho@kilala.vn>
2026-04-14 16:52:13 +0700

    560_subarray_sum_equals_k

---
+++ PREFIXSUM/560_Subarray_Sum_Equals_K/560_Subarray_Sum_Equals_K.java
@@ -0,0 +1,29 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+class SubarraySumEqualsK {
+
+    public int subarraySum(int[] nums, int k) {
+        if (nums == null) {
+            return 0;
+        }
+
+        Map<Integer, Integer> prefixMap = new HashMap<>(); // key: prefix - value: freq
+        prefixMap.put(0, 1); // xuất hiện 1 lần prefix 0 trước phần tử đầu tiên
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+
+            if (prefixMap.containsKey(currentSum - k)) { // trươcs đó có prefixSum nào = currentSum - k không?
+                count += prefixMap.getOrDefault(currentSum - k, 0);
+            }
+            prefixMap.put(currentSum, prefixMap.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return count;
+    }
+}
