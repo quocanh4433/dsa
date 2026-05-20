57424c9f021c0bf36708174058b02695731f0c4f
quocanh4433 <hoquocanh.td@gmail.com>
2026-05-20 16:51:10 +0700

    2144_minimum_cost_of_buying_candies_with_discount

---
+++ [N]greedy/2144_minimum_cost_of_buying_candies_with_discount/2144_minimum_cost_of_buying_candies_with_discount.java
@@ -0,0 +1,26 @@
+
+class MinimumCostOfBuyingCandiesWithDiscount {
+
+    /*
+        time O(nlogn)
+        space O(1)
+    
+     */
+    public int minimumCost(int[] cost) {
+        int n = cost.length;
+        int buy = 0;
+        int free = 0;
+
+        Arrays.sort(cost);
+
+        for (int i = 0; i < n; i++) {
+            buy += cost[i];
+        }
+
+        for (int i = n - 3; i >= 0; i -= 3) {
+            free += cost[i];
+        }
+
+        return buy - free;
+    }
+}
