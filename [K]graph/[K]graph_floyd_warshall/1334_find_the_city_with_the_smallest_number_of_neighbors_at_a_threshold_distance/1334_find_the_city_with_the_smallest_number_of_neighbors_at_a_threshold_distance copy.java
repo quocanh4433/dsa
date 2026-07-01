98521501c0280a12b4a9be649acd980949d5a5bd
quocanh4433 <hoquocanh.td@gmail.com>
2026-07-01 16:05:57 +0700

    1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance

--- [K]graph/[K]graph_floyd_warshall/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance copy.java
+++ [K]graph/[K]graph_floyd_warshall/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance copy.java
@@ -1,39 +1,46 @@
+
 import java.util.*;
 
-class Solution {
+class Solution_Dijkstra {
+
     /*
         time: O(n * elogn)
             ▪︎ build graph: O(e)
-            ▪︎ dijkstra: O(elogn)
-            ▪︎ duyện qua n node: O(n)
+            ▪︎ duyệt từng node và dijkstra: O(n * elogn)
+                ▪︎ duyện qua n node: O(n)
+                ▪︎ dijkstra O(elogn)
         space: O(e + n)
             ▪︎ graph: O(e + n)   
             ▪︎ dist[]: O(n)
             ▪︎ pq: O(n) 
-    */
+     */
+
+    private List<List<int[]>> graph;
+    private int n;
+    private int distanceThreshold;
 
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        this.n = n;
+        this.distanceThreshold = distanceThreshold;
         // build graph
-        List<int[]>[] graph = new ArrayList[n];
-
+        this.graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
+            graph.add(new ArrayList<>());
         }
 
         for (int[] e : edges) {
-            graph[e[0]].add(new int[]{e[1], e[2]});
-            graph[e[1]].add(new int[]{e[0], e[2]});
+            graph.get(e[0]).add(new int[]{e[1], e[2]});
+            graph.get(e[1]).add(new int[]{e[0], e[2]});
         }
 
         int city = -1; // thành phố cần trả về
         int minReachable = Integer.MAX_VALUE; // số thành phố ít nhất tưng thấy 
 
         for (int i = 0; i < n; i++) {
-            // số thành phố có thể đi bất đầu từ i thỏa điều kiện distanceThreshold
-            int count = dijkstra(i, graph, distanceThreshold, n);
+            // bắt đầu từ i đi qua
+            int count = dijkstra(i);
 
-            // tại sao điều kiện là <= mà không phải < ?
-            if (count <= minReachable) { 
+            if (count <= minReachable) {
                 minReachable = count;
                 city = i;
             }
@@ -42,12 +49,12 @@ class Solution {
         return city;
     }
 
-    public int dijkstra(int start, List<int[]>[] graph, int distanceThreshold, int n) {
+    public int dijkstra(int start) {
         int[] dist = new int[n]; // dist[i]: khoảng cách ngắn nhất từ start tới i
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[start] = 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
         pq.offer(new int[]{0, start});
 
         while (!pq.isEmpty()) {
@@ -55,9 +62,11 @@ class Solution {
             int d = curr[0];
             int u = curr[1];
 
-            if (d > dist[u]) continue;
+            if (d > dist[u]) {
+                continue;
+            }
 
-            for (int[] edge : graph[u]) {
+            for (int[] edge : graph.get(u)) {
                 int v = edge[0];
                 int w = edge[1];
                 if (dist[u] + w < dist[v]) {
---
+++ [K]graph/[K]graph_floyd_warshall/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance.java
@@ -0,0 +1,79 @@
+
+import java.util.Arrays;
+
+class Solution_Floyd {
+
+    /*
+        - matrix O(n**2)
+        - build graph O(e)
+        - floydwarshall O(n**3)
+
+        -> time O(n**3)
+
+
+        - dist[][] O(n**2)
+        - another variable O(1)
+
+        -> space O(n**2)
+    
+    
+     */
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int INF = Integer.MAX_VALUE;
+        int[][] dist = new int[n][n];
+
+        // initialize
+        for (int i = 0; i < n; i++) {
+            // thêm cho từng hàng
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        // build graph
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+
+            dist[u][v] = w;
+            dist[v][u] = w;
+        }
+
+        // floyd-warshall
+        for (int k = 0; k < n; k++) { // k là node trung gian ở giữa i và j
+            for (int i = 0; i < n; i++) { // i là node bắt đầu
+                for (int j = 0; j < n; j++) { // j là node kết thúc
+                    if (dist[i][k] == INF || dist[k][j] == INF) {
+                        // nếu không đi được từ i đến k hoặc không đi được từ k đến j, 
+                        // thì không thể đi từ i đến j thông qua k.
+                        continue;
+                    }
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        // result
+        int city = -1;
+        int minReachable = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (i != j && dist[i][j] <= distanceThreshold) {
+                    // đếm xem từ thành phố i có thể đi đến bao nhiêu thành phố khác với khoảng cách không vượt quá distanceThreshold.
+                    count++;
+                }
+            }
+
+            if (count <= minReachable) {
+                city = i;
+                minReachable = count;
+            }
+        }
+
+        return city;
+    }
+
+}
