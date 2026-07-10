c12bd31d4acabae1a9bb04a622779bff9b3c0e6a
quocanh4433 <hoquocanh.td@gmail.com>
2026-07-10 11:09:24 +0700

    1582_special_positions_in_a_binary_matrix

---
+++ [K]graph/[K]graph_matrix/1582_special_positions_in_a_binary_matrix/1582_special_positions_in_a_binary_matrix.java
@@ -0,0 +1,30 @@
+
+class SpecialPositionsInABinaryMatrix {
+        public int numSpecial(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int[] rows = new int[n];
+        int[] cols = new int[m];
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                if (mat[r][c] == 1) {
+                    rows[r]++;
+                    cols[c]++;
+                }
+            }
+        }
+
+        int res = 0;
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                if(mat[r][c] == 1 && rows[r] == 1 && cols[c] == 1) {
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
