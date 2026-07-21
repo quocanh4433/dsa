578fc668fb9256872ce85a36dae332cf36e1a0ef
quocanh4433 <hoquocanh.td@gmail.com>
2026-07-21 16:52:38 +0700

    609_find_duplicate_file_in_system

---
+++ [C]hashmap/609_find_duplicate_file_in_system/609_find_duplicate_file_in_system.java
@@ -0,0 +1,41 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class FindDuplicateFileInSystem {
+
+    public List<List<String>> findDuplicate(String[] paths) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String path : paths) {
+            String[] parts = path.split(" ");
+            String directory = parts[0];
+
+            for (int i = 1; i < parts.length; i++) {
+                String file = parts[i];
+
+                int idx = file.indexOf('(');
+
+                String fileName = file.substring(0, idx);
+                String content = file.substring(idx + 1, file.length() - 1);
+
+                String fullPath = directory + "/" + fileName;
+
+                map.computeIfAbsent(content, k -> new ArrayList<>())
+                        .add(fullPath);
+            }
+        }
+
+        List<List<String>> ans = new ArrayList<>();
+
+        for (List<String> list : map.values()) {
+            if (list.size() > 1) {
+                ans.add(list);
+            }
+        }
+
+        return ans;
+    }
+}
