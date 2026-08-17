e5ea20c78e67d50ef4b82688220017eba17f5702
quocanh4433 <hoquocanh.td@gmail.com>
2026-08-17 15:44:57 +0700

    819_most_common_word

--- [C]hashmap/2364_count_number_of_bad_pairs/2364_count_number_of_bad_pairs.java
+++ [C]hashmap/2364_count_number_of_bad_pairs/2364_count_number_of_bad_pairs.java
@@ -1,4 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
 
 class CountNumberOfBadPairs {
-    
+    /*
+        bad pair: j - i != nums[j] - nums[i]
+
+        good pair: j - nums[j] == i - nums[i] tương đương key = i - nums[i]
+
+        thay vì tìm bad pair ta tìm good pair
+
+        bad pair = tổng cặp mà i là phần tử thứ 2 - số good pair
+
+        tương đương
+
+        res += i - goodpair
+
+
+        time O(n)
+        space O(n)
+              
+
+     */
+    public long countBadPairs(int[] nums) {
+        // map để đếm số goodpairs của 1 key
+        Map<Integer, Integer> map = new HashMap<>();
+        long res = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            int key = i - nums[i];
+
+            int goodPairs = map.getOrDefault(key, 0);
+
+            res += i - goodPairs;
+
+            map.put(key, goodPairs + 1);
+        }
+
+        return res;
+    }
 }
---
+++ [C]hashmap/819_most_common_word/819_most_common_word.java
@@ -0,0 +1,33 @@
+class MostCommonWord {
+    /*
+        time O(n)
+        space O(n)
+     */
+    public String mostCommonWord(String paragraph, String[] banned) {
+        // 1. đưa banned vao set để kiểm tra
+        Set<String> bannedSet = new HashSet<>();
+        for(String ban : banned) {
+            bannedSet.add(ban);
+        }
+
+
+        // 2. đếm số lần xuất hiện
+        Map<String, Integer> freqMap = new HashMap<>();
+        for(String str : paragraph.toLowerCase().split("[^a-z]+")) { //  → tách mọi thứ không phải chữ cái
+            if(bannedSet.contains(str))  continue;
+            freqMap.put(str, freqMap.getOrDefault(str, 0) + 1);
+        }
+
+
+        // 3. tìm từ frequency nhiểu nhất
+        String res = "";
+
+        for(String key : freqMap.keySet()) {
+            if(res.isEmpty() || freqMap.get(key) > freqMap.get(res)) {
+                res = key;
+            }
+        }
+
+        return res;
+    }
+}
