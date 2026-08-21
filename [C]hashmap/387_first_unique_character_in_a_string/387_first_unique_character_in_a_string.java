962475f817809a0f1b2f17ef77c5d33bff535717
quocanh4433 <hoquocanh.td@gmail.com>
2026-08-21 09:25:07 +0700

    387_first_unique_character_in_a_string

--- [C]hashmap/387_first_unique_character_in_a_string/387_first_unique_character_in_a_string.java
+++ [C]hashmap/387_first_unique_character_in_a_string/387_first_unique_character_in_a_string.java
@@ -1,4 +1,24 @@
 
 class FirstUniqueCharacterInAString {
-    
+
+    /*
+        time O(n)
+        space O(1)
+     */
+    public int firstUniqChar(String s) {
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (count[ch - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
