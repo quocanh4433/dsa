2852487a7497b477a7e91e9588328529b72beeeb
quocanh4433 <hoquocanh.td@gmail.com>
2026-08-27 16:06:40 +0700

    75_sort_color

--- [H]array/[H]array-two-pointers-same-direction/75_sort_color/75_sort_color_onepass.java
+++ [H]array/[H]array-two-pointers-same-direction/75_sort_color/75_sort_color_onepass.java
@@ -4,8 +4,33 @@ class SortColor_OnePass {
     /*
         1 pass
         
+        time O(n)
+        space O(3) ~ O(1)
+    
      */
     public void sortColors(int[] nums) {
-        
+        int low = 0; // điểm giới hạn của vùng số 0
+        int mid = 0; // đoạn mid đến high là vung cần kiểm tra
+        int high = nums.length - 1; // điểm giới hạn của vùng số 2
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, mid, low);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+                // Lưu ý: Không tăng mid ở đây vì số mới swap từ high về mid chưa được kiểm tra!
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
