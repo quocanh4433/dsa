de7ec566063b19cf5708c6b555401673335e8e7c
quocanh4433 <hoquocanh.td@gmail.com>
2026-09-16 15:41:08 +0700

    80_remove_duplicates_from_sorted_array_ii

--- [H]array/[H]array_two_pointers_same_direction/26_remove_duplicates_from_sorted_array/26_remove_duplicates_from_sorted_array.java
+++ [H]array/[H]array_two_pointers_same_direction/26_remove_duplicates_from_sorted_array/26_remove_duplicates_from_sorted_array.java
@@ -1,4 +1,22 @@
 
 class RemoveDuplicatesFromSortedArray {
-    
+    /* 
+        time O(n)
+        space O(1)
+    */
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int write = 1;
+        for (int read = 1; read < nums.length; read++) {
+            if (nums[read] != nums[write - 1]) {
+                nums[write] = nums[read];
+                write++;
+            }
+        }
+
+        return write;
+    }
 }
