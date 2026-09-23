cba366be8223d398087546bae369f1059c16a764
quocanh4433 <hoquocanh.td@gmail.com>
2026-09-23 16:44:19 +0700

    1710_maximum_units_on_a_truck

--- [H]array/[H]array_sort/1710_maximum_units_on_a_truck/1710_maximum_units_on_a_truck.java
+++ [H]array/[H]array_sort/1710_maximum_units_on_a_truck/1710_maximum_units_on_a_truck.java
@@ -1,4 +1,32 @@
 
 class MaximumUnitsOnATsruck {
-    
+
+    /*
+        time O(n)
+        space O(1)
+     */
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        int[] bucket = new int[1001];
+
+        // Put boxes into buckets
+        for (int[] box : boxTypes) {
+            int numberOfBoxes = box[0];
+            int unitsPerBox = box[1];
+
+            bucket[unitsPerBox] += numberOfBoxes;
+        }
+
+        int res = 0;
+
+        for (int units = 1000; units >= 1 && truckSize > 0; units--) {
+
+            int boxes = Math.min(bucket[units], truckSize);
+
+            res += boxes * units;
+
+            truckSize -= boxes;
+        }
+
+        return res;
+    }
 }
