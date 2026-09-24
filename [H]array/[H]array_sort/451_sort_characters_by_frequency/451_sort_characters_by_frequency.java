bfdafb984ac9475494faf66c6bb3165722f90376
quocanh4433 <hoquocanh.td@gmail.com>
2026-09-24 11:16:59 +0700

    451_sort_characters_by_frequency

---
+++ [H]array/[H]array_sort/451_sort_characters_by_frequency/451_sort_characters_by_frequency.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class SortCharactersByFrequency {
+    /**
+        time O(n)
+        space O(n)
+    
+     */
+    public String frequencySort(String s) {
+        int n = s.length();
+
+        // 1. đếm freq
+        Map<Character, Integer> freqs = new HashMap<>();
+
+        for(char ch : s.toCharArray()) {
+            freqs.put(ch, freqs.getOrDefault(ch, 0) + 1);
+        }
+
+        // 2. tạo bucket
+        List<List<Character>> buckets = new ArrayList<>(n + 1);
+
+        for (int i = 0; i <= n; i++) {
+            buckets.add(new ArrayList<>());
+        }
+
+        for(char key : freqs.keySet()) {
+            int freq = freqs.get(key);
+            buckets.get(freq).add(key);
+        }
+
+
+        // 2. build từ bucket có freq lớn đến nhỏ
+        StringBuilder sb = new StringBuilder();
+        for(int freq = n; freq >= 1; freq--) {
+            for(char ch : buckets.get(freq)) {
+                for(int i = 0; i < freq; i++) {
+                    sb.append(ch);
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
